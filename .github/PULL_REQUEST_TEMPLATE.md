5c7f8e264decc075b9fb0dd53946ceb70e9aa9ab
kagorora <kagororamaxime@gmail.com>
2020-07-30 18:40:09 +0200

    ft(Login): created Login Api
    [Finishes: #173995821]

--- .github/PULL_REQUEST_TEMPLATE.md
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -1,6 +1,24 @@
 #### What does this PR do?
 #### Description of Task to be completed?
 #### How should this be manually tested?
+
+- Clone the project through your terminal(VScode terminal)
+
+      git clone https://github.com/Kagorora/DMS-Debt-Managment-System-.git
+
+- Open the project 
+
+       cd DMS-Debt-Managment-System-.git && cd .
+
+- Open this feature branch
+
+       git checkout ft-signup-backend-173995812
+
+- Install all the packages
+
+       npm i
+
+
 #### Any background context you want to provide?
 #### What are the relevant pivotal tracker stories?
 #### Screenshots (if appropriate)
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import bcrypt from 'bcrypt';
 import { User } from '../database/models';
+import userSchema from '../helper/loginValidation';
 import generateToken from '../helper/tokenGenerator';
 
 dotenv.config();
@@ -9,6 +10,13 @@ class userController {
   static async signup(req, res) {
     try {
       const { email, password, confirmPassword } = req.body;
+      const user = userSchema.validate({ email, password });
+
+      if (user.error) {
+        return res
+          .status(400)
+          .json({ status: 400, error: user.error.details[0].message });
+      }
       const existingUser = await User.findOne({ where: { email } });
       if (existingUser !== null) return res.status(409).json({ error: 'Email has already taken.' });
       if (password !== confirmPassword) {
@@ -23,10 +31,46 @@ class userController {
       });
       const newUser = await User.create(newData);
       return res.status(201).json({
-        status: 200,
-        jwtoken: token,
+        status: 201,
         message: 'new user successfuly registered',
+        jwtoken: token,
         data: newUser,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  static async login(req, res) {
+    try {
+      const { email, password } = req.body;
+      const user = userSchema.validate({ email, password });
+
+      if (user.error) {
+        return res
+          .status(400)
+          .json({ status: 400, error: user.error.details[0].message });
+      }
+      const existingUser = await User.findOne({ where: { email } });
+      if (existingUser == null) {
+        return res
+          .status(404)
+          .json({ status: 400, error: 'Email or password is incorrect' });
+      }
+      const passwordMatch = await bcrypt.compare(
+        password,
+        existingUser.password,
+      );
+      if (passwordMatch === false) {
+        return res
+          .status(400)
+          .json({ status: 400, error: 'Email or password is incorrect' });
+      }
+      const token = generateToken({ email });
+      return res.status(200).json({
+        status: 200,
+        message: 'user successfuly registered',
+        jwtoken: token,
       });
     } catch (err) {
       console.log(err);
